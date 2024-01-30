608392cc48926861afdddbade7819a8be1af2d5b
SathyaAk9498 <137285362+SathyaAk9498@users.noreply.github.com>
2024-01-30 15:31:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var a1=document.getElementById("d1");
+var a2=document.getElementById("p1");
+var a3=document.getElementById("d2");
+var a4=document.getElementById("p2");
+// syntax:element.addEventListener(event,function usecapture)
+a1.addEventListener("click",a)
+function a(){
+a3.style.backgroundColor="yellow"
+}
